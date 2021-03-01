f671284c9e1e891dcd1ceb91a877d6f928b4e25a
Andreza Feu <andrezafeu@gmail.com>
2021-03-01 16:50:10 -0500

    Add vue instance

---
+++ app.js
@@ -0,0 +1,9 @@
+// vue instance
+const helloWorld = new Vue({
+    // el: vue should attach or mount itself to this html element
+    el: '#helloVue',
+    data: {
+        title: "Hello world!!!",
+        message: "This is my vue template"
+    }
+});
